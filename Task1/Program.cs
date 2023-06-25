52e081dd1e0f29479348ee837aa8699e45e19a4c
Dmitrii Priimak <priimak69@inbox.ru>
2023-06-25 15:35:15 +0700

    Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,4 +2,36 @@
 Задайте массив заполненный случайными положительными трёхзначными числами. 
 Напишите программу, которая покажет количество чётных чисел в массиве.
 */
-Console.WriteLine("Hello, World!");
+int [] CreatArrey (int size, int minVal, int maxVal)
+{
+    int [] newArrey = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        newArrey[i] = new Random().Next(minVal, maxVal + 1);
+    }
+    return newArrey;
+}
+void ShowArrey (int [] arreyToShow)
+{
+    for (int i = 0; i < arreyToShow.Length; i++)
+    Console.Write (arreyToShow[i] + " ");
+    Console.WriteLine();
+}
+int SumElem (int [] elemSum)
+{
+    int sum = 0;
+    for (int i = 0; i < elemSum.Length; i++)
+        if (elemSum[i] % 10 == 0)
+        sum++;
+    Console.Write ($"number of even elements: {sum}");
+    return sum;
+}
+
+Console.WriteLine ("Input size of arrey: ");
+int sizeArr = Convert.ToInt32(Console.ReadLine());
+int minArrVal = 100;
+int maxArrVal = 1000;
+int [] finArrey = CreatArrey(sizeArr, minArrVal, maxArrVal);
+ShowArrey (finArrey);
+int finSum = SumElem (finArrey);
+
